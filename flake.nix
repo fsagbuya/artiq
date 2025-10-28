cc6684fc7cda6a4354ca2156050ce4ef568eb09c
fsagbuya <fa@m-labs.ph>
2025-10-28 11:11:55 +0800

    add pythonpath warning 4

--- flake.nix
+++ flake.nix
@@ -502,6 +502,7 @@
             packages.x86_64-linux.vivado
             packages.x86_64-linux.openocd-bscanspi
           ];
+           
         shellHook = ''
           export LIBARTIQ_SUPPORT=`libartiq-support`
           export QT_PLUGIN_PATH=${qtPaths.QT_PLUGIN_PATH}
