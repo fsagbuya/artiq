967d4ec736a6587ec6dd7dfe0e344c88d3583f1b
fsagbuya <fa@m-labs.ph>
2025-11-06 17:39:50 +0800

    flake: refactor artiq packaging to support minimal builds

--- flake.nix
+++ flake.nix
@@ -162,53 +162,74 @@
       '';
     };
 
-    artiq = pkgs.python3Packages.buildPythonPackage rec {
-      pname = "artiq";
-      version = artiqVersion;
-      src = self;
-      pyproject = true;
-      build-system = [pkgs.python3Packages.setuptools];
+    makeArtiqPackage = { withMinimalDeps ? false }:
+      pkgs.python3Packages.buildPythonPackage rec {
+        pname = "artiq";
+        version = artiqVersion;
+        src = self;
+        pyproject = true;
+        build-system = [pkgs.python3Packages.setuptools];
 
-      preBuild = ''
-        export VERSIONEER_OVERRIDE=${version}
-        export VERSIONEER_REV=${artiqRev}
-      '';
+        preBuild = ''
+          export VERSIONEER_OVERRIDE=${version}
+          export VERSIONEER_REV=${artiqRev}
+        '';
 
-      nativeBuildInputs = [pkgs.qt6.wrapQtAppsHook];
-      propagatedBuildInputs =
-        [pkgs.llvm_20 pkgs.lld_20 sipyco.packages.x86_64-linux.sipyco pythonparser pkgs.qt6.qtsvg artiq-comtools.packages.x86_64-linux.artiq-comtools]
-        ++ (with pkgs.python3Packages; [llvmlite pyqtgraph pygit2 numpy python-dateutil scipy prettytable pyserial levenshtein h5py pyqt6 qasync tqdm lmdb jsonschema platformdirs]);
+        nativeBuildInputs = pkgs.lib.optionals (!withMinimalDeps) [
+          pkgs.qt6.wrapQtAppsHook
+        ];
+        propagatedBuildInputs = [
+          sipyco.packages.x86_64-linux.sipyco pkgs.python3Packages.jsonschema
+        ]
+        ++ pkgs.lib.optionals (!withMinimalDeps) [
+          pkgs.llvm_20 pkgs.lld_20 pythonparser pkgs.qt6.qtsvg
+          artiq-comtools.packages.x86_64-linux.artiq-comtools
+        ]
+        ++ (with pkgs.python3Packages; pkgs.lib.optionals (!withMinimalDeps) [
+          llvmlite pyqtgraph pygit2 numpy python-dateutil scipy prettytable
+          pyserial levenshtein h5py pyqt6 qasync tqdm lmdb platformdirs
+        ]);
 
-      dontWrapQtApps = true;
-      postFixup = ''
-        wrapQtApp "$out/bin/artiq_dashboard"
-        wrapQtApp "$out/bin/artiq_browser"
-        wrapQtApp "$out/bin/artiq_session"
-      '';
+        dontWrapQtApps = true;
+        postFixup = pkgs.lib.optionalString (!withMinimalDeps) ''
+          wrapQtApp "$out/bin/artiq_dashboard"
+          wrapQtApp "$out/bin/artiq_browser"
+          wrapQtApp "$out/bin/artiq_session"
+        '';
 
-      preFixup = ''
-        # Ensure that wrapProgram uses makeShellWrapper rather than makeBinaryWrapper
-        # brought in by wrapQtAppsHook. Only makeShellWrapper supports --run.
-        wrapProgram() { wrapProgramShell "$@"; }
-      '';
-      ## Modifies PATH to pass the wrapped python environment (i.e. python3.withPackages(...) to subprocesses.
-      ## Allows subprocesses using python to find all packages you have installed
-      makeWrapperArgs = [
-        ''--run 'if [ ! -z "$NIX_PYTHONPREFIX" ]; then export PATH=$NIX_PYTHONPREFIX/bin:$PATH;fi' ''
-        "--set FONTCONFIG_FILE ${pkgs.fontconfig.out}/etc/fonts/fonts.conf"
-      ];
+        preFixup = pkgs.lib.optionalString (!withMinimalDeps) ''
+          # Ensure that wrapProgram uses makeShellWrapper rather than makeBinaryWrapper
+          # brought in by wrapQtAppsHook. Only makeShellWrapper supports --run.
+          wrapProgram() { wrapProgramShell "$@"; }
+        '';
+        ## Modifies PATH to pass the wrapped python environment (i.e. python3.withPackages(...) to subprocesses.
+        ## Allows subprocesses using python to find all packages you have installed
+        makeWrapperArgs = pkgs.lib.optionals (!withMinimalDeps) [
+          ''--run 'if [ ! -z "$NIX_PYTHONPREFIX" ]; then export PATH=$NIX_PYTHONPREFIX/bin:$PATH;fi' ''
+          "--set FONTCONFIG_FILE ${pkgs.fontconfig.out}/etc/fonts/fonts.conf"
+        ];
 
-      # FIXME: automatically propagate lld_20 llvm_20 dependencies
-      # cacert is required in the check stage only, as certificates are to be
-      # obtained from system elsewhere
-      nativeCheckInputs = with pkgs; [lld_20 llvm_20 lit outputcheck cacert] ++ [libartiq-support];
-      checkPhase = ''
-        python -m unittest discover -v artiq.test
+        dontCheckRuntimeDeps = withMinimalDeps;
+        # FIXME: automatically propagate lld_20 llvm_20 dependencies
+        # cacert is required in the check stage only, as certificates are to be
+        # obtained from system elsewhere
+        nativeCheckInputs = pkgs.lib.optionals (!withMinimalDeps) [
+          pkgs.lld_20 pkgs.llvm_20 pkgs.lit pkgs.outputcheck pkgs.cacert libartiq-support
+        ];
+        checkPhase = pkgs.lib.optionalString (!withMinimalDeps) ''
+          python -m unittest discover -v artiq.test
 
-        TESTDIR=`mktemp -d`
-        cp --no-preserve=mode,ownership -R $src/artiq/test/lit $TESTDIR
-        LIBARTIQ_SUPPORT=`libartiq-support` lit -v $TESTDIR/lit
-      '';
+          TESTDIR=`mktemp -d`
+          cp --no-preserve=mode,ownership -R $src/artiq/test/lit $TESTDIR
+          LIBARTIQ_SUPPORT=`libartiq-support` lit -v $TESTDIR/lit
+        '';
+      };
+
+    artiq = makeArtiqPackage {};
+
+    # Stripped down version of ARTIQ for gateware/firmware builds
+    artiq-build = makeArtiqPackage {
+      withMinimalDeps = true;
     };
 
     migen = pkgs.python3Packages.buildPythonPackage rec {
@@ -380,7 +401,7 @@
       '';
   in rec {
     packages.x86_64-linux = {
-      inherit pythonparser qasync artiq;
+      inherit pythonparser qasync artiq artiq-build;
       inherit migen misoc asyncserial microscope vivadoEnv vivado;
       openocd-bscanspi = openocd-bscanspi-f pkgs;
       artiq-board-kc705-nist_clock = makeArtiqBoardPackage {
